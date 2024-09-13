918030aabbfaa6617fdf21ab2129eb469557c935
praveen212206 <167956964+praveen212206@users.noreply.github.com>
2024-09-13 19:18:33 +0530

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,31 @@
-#!/usr/bin/env python
-
-"""Top-level script to invoke helloworld implementation."""
-
+import argparse
 import sys
-import helloworld.main
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello, World!"
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='A simple example program to print a friendly greeting and start a server.')
+    parser.add_argument('--port', type=int, default=5000, help='Port to run the server on')
+    parser.add_argument('--version', action='version', version='helloworld ' + '1.0.0')  # Replace '1.0.0' with actual version
+    return parser.parse_args()
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+
+    args = parse_args()
+
+    # If the --port argument is provided, start the Flask server
+    if '--port' in argv:
+        app.run(host='0.0.0.0', port=args.port)
+    else:
+        print("Hello, world")
+
+    return 0
 
 if __name__ == '__main__':
-    sys.exit(helloworld.main.main())
+    sys.exit(main())
